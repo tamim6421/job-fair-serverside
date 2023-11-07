bfa542983eeee19aa2b9a8b7dda455e70cd96b91
ProgrammingHero <tamimhossain6421@gmail.com>
2023-11-08 02:18:04 +0600

    try to solve jwt problem

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const {
+  MongoClient,
+  ServerApiVersion,
+  Collection,
+  ObjectId,
+} = require("mongodb");
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
-require('dotenv').config()
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000 
-
-
-// middleware 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true,
-}))
-app.use(express.json())
-app.use(cookieParser())
-
-
-
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.PROJECT_NAME}:${process.env.PROJECT_PASS}@cluster0.iimwc2a.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,33 +31,54 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("middleware token", token);
 
-const verifyToken = async(req, res, next) =>{
-  const token = req.cookies?.token 
-  console.log('middleware token', token)
-
-  if(!token){
-      return res.status(401).send({message:'not authorized'})
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-      // error 
-      if(err){
-          return res.status(401).send({message: 'unauthorized'})
-      }
-      console.log('decoded', decoded)
-      req.user = decoded
-      next()
-  })
- 
-} 
 
-const logger = (req, res, next) =>{
-  console.log('log Infu ',req.method, req.url)
-  next()
-}
+  jwt.verify(token,process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      if (err.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .send({ message: "Token has expired. Please log in again." });
+      }
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+// const verifyToken = async(req, res, next) =>{
+//   const token = req.cookies?.token
+//   console.log('middleware token', token)
+
+//   if(!token){
+//       return res.status(401).send({message:'not authorized'})
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//       // error
+//       if(err){
+//           return res.status(401).send({message: 'unauthorized'})
+//       }
+//       console.log('decoded', decoded)
+//       req.user = decoded
+//       next()
+//   })
+
+// }
+
+const logger = (req, res, next) => {
+  console.log("log Infu ", req.method, req.url);
+  next();
+};
 
 async function run() {
   try {
@@ -63,183 +86,166 @@ async function run() {
     await client.connect();
 
     // database Collection
-    const categoryCollections = client.db('job_fair').collection('services')
-    const jobsCollections = client.db('job_fair').collection('all_jobs')
-    const bidProjectCollection = client.db('job_fair').collection('bidData')
-   
+    const categoryCollections = client.db("job_fair").collection("services");
+    const jobsCollections = client.db("job_fair").collection("all_jobs");
+    const bidProjectCollection = client.db("job_fair").collection("bidData");
 
+    // token related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1hr",
+      });
+      res
+        .cookie("token", token, { httpOnly: true, secure: false })
+        .send({ success: true });
+    });
 
-  // token related api 
-  app.post('/jwt', async(req, res) =>{
-    const user = req.body
-    console.log("user for token" ,user)
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1hr'})
-    res.
-    cookie('token', token, {httpOnly: true, secure: true,})
-    .send({success: true})
-})
+    // remove token in logout user
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("loggout user", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
 
+    // get category
+    app.get("/category", async (req, res) => {
+      const cursor = categoryCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // post add jobs in the database
+    app.post("/jobs", async (req, res) => {
+      const jobs = req.body;
+      console.log(jobs);
+      const result = await jobsCollections.insertOne(jobs);
+      res.send(result);
+    });
 
-// remove token in logout user 
-app.post('/logout', async(req, res) =>{
-  const user = req.body
-  console.log('loggout user', user)
-  res.clearCookie('token', {maxAge: 0}).send({success: true})
-})
+    // update added  job data
+    app.put("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateJobs = req.body;
+      // console.log(updateJobs)
+      const option = { upsert: true };
+      const jobs = {
+        $set: {
+          jobTitle: updateJobs.jobTitle,
+          deadline: updateJobs.deadline,
+          priceRange: updateJobs.priceRange,
+          shortDescription: updateJobs.shortDescription,
+          category: updateJobs.category,
+          maximumPrice: updateJobs.maximumPrice,
+          minimumPrice: updateJobs.minimumPrice,
+        },
+      };
+      const result = await jobsCollections.updateOne(filter, jobs, option);
+      res.send(result);
+    });
 
+    // delete posted job
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.deleteOne(query);
+      res.send(result);
+    });
 
+    // get all jobs by using category and email
+    app.get("/jobs", verifyToken, async (req, res) => {
+      // console.log('req info', req.user)
+      console.log("cooooook", req.cookies);
 
-// get category 
-app.get('/category', async(req, res) =>{
-    const cursor = categoryCollections.find()
-    const result = await cursor.toArray()
-    res.send(result)
-})
+      const employerEmail = req.query.employerEmail;
+      const category = req.query.category;
 
+      // if(req.user.email !== req.query.employerEmail){
+      //   return res.status(403).send({message: 'Forbidden Access'})
+      // }
 
+      let query = {};
+      if (employerEmail) {
+        query.employerEmail = employerEmail;
+      }
 
-    // post add jobs in the database 
-    app.post('/jobs', async(req, res) =>{
-        const jobs = req.body 
-        console.log(jobs)
-        const result = await jobsCollections.insertOne(jobs)
-        res.send(result)
-    })
+      let catQueryObj = {};
+      if (category) {
+        catQueryObj.category = category;
+      }
 
+      // console.log('email', query);
+      // console.log('cat', catQueryObj);
 
+      const cursor = jobsCollections.find({ ...catQueryObj, ...query });
+      const result = await cursor.toArray();
 
-    // update added  job data 
-app.put('/jobs/:id', async(req, res) =>{
-  const id = req.params.id 
-  const filter = {_id: new ObjectId(id)}
-  const updateJobs = req.body 
-  // console.log(updateJobs)
-  const option = {upsert: true}
-  const jobs ={
-    $set:{
-      jobTitle:updateJobs.jobTitle,
-       deadline:updateJobs.deadline,
-       priceRange:updateJobs.priceRange,
-       shortDescription:updateJobs.shortDescription,
-       category:updateJobs.category,
-       maximumPrice:updateJobs.maximumPrice,
-       minimumPrice:updateJobs.minimumPrice
+      res.send(result);
+    });
 
+    // get job by id
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.findOne(query);
+      res.send(result);
+    });
+
+    // post bid on the Project data
+    app.post("/bidProject", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await bidProjectCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // get my bids all jobs
+    app.get("/bidProject", verifyToken, async (req, res) => {
+      const employerEmail = req.query.employerEmail;
+      console.log('employer email', employerEmail)
+      const email = req.query.email;
+    console.log(req.query)
+    console.log('user', req.user)
+
+    if(req.query.email !== req.user.email){
+      return res.status(403).send({message: 'forbidden access'})
     }
-  }
-  const result = await jobsCollections.updateOne(filter, jobs, option)
-  res.send(result)
-})
 
+      let query = {};
+      if (email) {
+        query.email = email;
+      }
+      console.log("email", query);
 
-// delete posted job 
-app.delete('/jobs/:id', async(req, res) =>{
-  const id = req.params.id 
-  const query = {_id: new ObjectId(id)}
-  const result = await jobsCollections.deleteOne(query)
-  res.send(result)
-})
+      const cursor = bidProjectCollection.find({ ...query }).sort({status: 1});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // update job status with click Bid request reject button
 
-
-// get all jobs by using category and email
-app.get('/jobs',logger, async (req, res) => {
-  // console.log('req info', req.user)
-  console.log('cooooook', req.cookies)
-
-  const employerEmail = req.query.employerEmail;
-  const category = req.query.category;
-
-
-  // if(req.user.email !== req.query.employerEmail){
-  //   return res.status(403).send({message: 'Forbidden Access'})
-  // }
-
-
-  let query = {};
-  if (employerEmail) {
-    query.employerEmail = employerEmail;
-  }
-
-  let catQueryObj = {};
-  if (category) {
-    catQueryObj.category = category;
-  }
-
-  // console.log('email', query);
-  // console.log('cat', catQueryObj);
-
-  const cursor = jobsCollections.find({ ...catQueryObj, ...query });
-  const result = await cursor.toArray();
-
-  res.send(result);
-});
-
-
-
-
-
-
-
-// get job by id 
-app.get('/jobs/:id', async(req, res) =>{
-  const id = req.params.id 
-  const query = {_id: new ObjectId(id)}
-  const result = await jobsCollections.findOne(query)
-  res.send(result)
-})
-
-
-
-
-// post bid on the Project data 
-app.post('/bidProject', async(req, res) =>{
-  const data = req.body 
-  console.log(data)
-  const result = await bidProjectCollection.insertOne(data)
-  res.send(result)
-})
-
-
-
-// get my bids all jobs 
-app.get('/bidProject', async(req, res) =>{
-  const email = req.query.email;
-
-  let query = {};
-  if (email) {
-    query.email = email;
-  }
-console.log('email', query)
-
-  const cursor = bidProjectCollection.find({...query})
-  const result = await cursor.toArray()
-  res.send(result)
-})
-
-
-// update job status with click Bid request reject button 
-
-app.patch('/bidProject/:id', async(req, res) =>{
-  const id = req.params.id 
-  const filter ={_id: new ObjectId(id)}
-  const updateStatus = req.body 
-  const updateDoc = {
-    $set:{
-      status:updateStatus.status
-    }
-  }
-  console.log(updateStatus)
-  const result = await bidProjectCollection.updateOne(filter, updateDoc)
-  res.send(result)
-})
-
-
+    app.patch("/bidProject/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateStatus = req.body;
+      console.log(req.body);
+      const updateDoc = {
+        $set: {
+          status: updateStatus.status,
+        },
+      };
+      console.log(updateStatus);
+      const result = await bidProjectCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -247,12 +253,10 @@ app.patch('/bidProject/:id', async(req, res) =>{
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("JOB FAIR");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('JOB FAIR')
-  })
-  
-  app.listen(port, () => {
-    console.log(`PORI IS RUNNING  ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`PORI IS RUNNING  ${port}`);
+});
